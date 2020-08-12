ecbd6f096ab0f482cf3275480622b4f5180a30d8
Aman Almadi <amanalmadi23@gmail.com>
2020-08-12 13:11:49 +0530

    Initial commit

---
+++ assignment8/Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawner : MonoBehaviour
+{
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine(SpawnGems());
+	}
+
+	IEnumerator SpawnGems()
+	{
+		while (true)
+		{
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			yield return new WaitForSeconds(Random.Range(5, 10));
+		}
+	}
+}
